429994cb76bd6538c6ddc261e74681c6e00c031a
mozammal89 <mozammal.bubt89@gmail.com>
2022-08-31 16:34:35 +0600

    feature prodect section design

--- src/components/home/FeatureProducts.jsx
+++ src/components/home/FeatureProducts.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 class FeatureProducts extends Component {
   render() {
@@ -12,8 +12,60 @@ class FeatureProducts extends Component {
 
 
           <Row>
-            <Col>
+            <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
+              <Card className='image-box'>
+                <img className='center' src="https://www.mobiledokan.com/wp-content/uploads/2022/05/Samsung-Galaxy-F23-5G.jpg" />
+                <Card.Body>
+                  <p className='product-name-on-card'>Samsung Galaxy F23 5G</p>
+                  <p className='product-price-on-card'>Price : BDT 3000</p>
+                </Card.Body>
+              </Card>
+            </Col>
 
+            <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
+              <Card className='image-box'>
+                <img className='center' src="https://www.mobiledokan.com/wp-content/uploads/2022/05/Samsung-Galaxy-M33.jpg" />
+                <Card.Body>
+                  <p className='product-name-on-card'>Samsung Galaxy M33 5G</p>
+                  <p className='product-price-on-card'>Price : BDT 3000</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
+              <Card className='image-box'>
+                <img className='center' src="https://www.mobiledokan.com/wp-content/uploads/2022/08/Samsung-Galaxy-Z-Fold-4.jpg" />
+                <Card.Body>
+                  <p className='product-name-on-card'>Samsung Galaxy Z Fold4</p>
+                  <p className='product-price-on-card'>Price : BDT 3000</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
+              <Card className='image-box'>
+                <img className='center' src="https://www.mobiledokan.com/wp-content/uploads/2022/08/Samsung-Galaxy-Z-Flip4.jpg" />
+                <Card.Body>
+                  <p className='product-name-on-card'>Samsung Galaxy Z Flip4</p>
+                  <p className='product-price-on-card'>Price : BDT 3000</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
+              <Card className='image-box'>
+                <img className='center' src="https://www.mobiledokan.com/wp-content/uploads/2022/08/Samsung-Galaxy-F13.jpg" />
+                <Card.Body>
+                  <p className='product-name-on-card'>Samsung Galaxy F13 </p>
+                  <p className='product-price-on-card'>Price : BDT 3000</p>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
+              <Card className='image-box'>
+                <img className='center' src="https://www.mobiledokan.com/wp-content/uploads/2022/07/Samsung-Galaxy-M53-5G.jpg" />
+                <Card.Body>
+                  <p className='product-name-on-card'>Samsung Galaxy M53 5G</p>
+                  <p className='product-price-on-card'>Price : BDT 3000</p>
+                </Card.Body>
+              </Card>
             </Col>
           </Row>
 
