5d35e30bab81023e40df2c7478ce1fdbe1176f3d
mozammal89 <mozammal.bubt89@gmail.com>
2022-09-01 01:35:35 +0600

    add sidebar, & home page slider

--- src/components/home/MegaMenu.jsx
+++ src/components/home/MegaMenu.jsx
@@ -1,9 +1,171 @@
 import React, { Component } from 'react'
 
- class MegaMenu extends Component {
+class MegaMenu extends Component {
+
+  constructor(){
+    super();
+    this.MegaMenu = this.MegaMenu.bind(this);
+
+  }
+
+  componentDidMount(){
+    this.MegaMenu();
+  }
+
+  MegaMenu(){
+    var acc = document.getElementsByClassName("accordion");
+    var accNum = acc.length;
+    var i;
+          for(i=0;i<accNum;i++){
+               acc[i].addEventListener("click",function (){
+                    this.classList.toggle("active");
+                    var panel = this.nextElementSibling;
+                    if(panel.style.maxHeight){
+                         panel.style.maxHeight = null;
+                    }else{
+                         panel.style.maxHeight= panel.scrollHeight+ "px"
+                    }
+               })
+    }
+  }
   render() {
     return (
-      <div>MegaMenu</div>
+        <div className='accordionMenuDiv'>
+          <div className='accordionMenuDivInside'>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+
+
+            <button className='accordion'>
+              <img className='accordionMenuIcon' src='https://cdn-icons-png.flaticon.com/128/739/739249.png' />&nbsp;
+              Man's Clothing
+            </button>
+            <div className='panel'>
+              <ul>
+                <li><a href='' className='accordionItem'>Man's Tshirt 1</a></li>
+                <li><a href='' className='accordionItem'>Man's Tshirt 2</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
     )
   }
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React, { Component, Fragment } from 'react'
 import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import FeatureProducts from '../components/home/FeatureProducts'
+import HomeTop from '../components/home/HomeTop'
 import NewArrival from '../components/home/NewArrival'
 
  class HomePage extends Component {
   render() {
     return (
       <Fragment>
+        <HomeTop />
         <FeatureProducts />
         <NewArrival />
         <Collection />
