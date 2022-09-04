92c9b58e59accd91b11387c658bbb092998e9777
mozammal89 <mozammal.bubt89@gmail.com>
2022-09-05 00:54:01 +0600

    notification page design

--- src/route/AppRoute.js
+++ src/route/AppRoute.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { Routes, Route, Link } from "react-router-dom";
 import ContactPage from '../pages/ContactPage';
 import HomePage from '../pages/HomePage';
+import NotificationPage from '../pages/NotificationPage';
 import PrivacyPage from '../pages/PrivacyPage';
 import ProductDetailsPage from '../pages/ProductDetailsPage';
 import PurchasePage from '../pages/PurchasePage';
@@ -20,6 +21,7 @@ import UserLoginPage from '../pages/UserLoginPage';
           <Route path="/privacy" element={<PrivacyPage />} />
           <Route path="/refund" element={<RefundPage />} />
           <Route path="/productdetails" element={<ProductDetailsPage />} />
+          <Route path="/notification" element={<NotificationPage />} />
         </Routes>
         
       </Fragment>
