7fe1555f66981a80604f3932ebce4f8ab5c9e942
mozammal89 <mozammal.bubt89@gmail.com>
2022-09-04 16:53:20 +0600

    desktop menu and mobile menu setup

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -2,8 +2,40 @@ import React, { Component, Fragment } from 'react'
 import { Navbar, Container, Row, Button, Col } from 'react-bootstrap'
 import Logo from '../../assets/images/easyshop.png';
 import { Link } from "react-router-dom";
+import MegaMenuAll from '../home/MegaMenuAll';
 
 class NavMenuDesktop extends Component {
+
+  constructor(){
+    super();
+    this.state={
+         SideNavState: "sideNavClose",
+         ContentOverState: "ContentOverlayClose"
+    }
+}
+
+
+MenuBarClickHandler=()=>{
+    this.SideNavOpenClose();
+}
+
+ContentOverlayClickHandler=()=>{
+    this.SideNavOpenClose();
+}
+
+
+SideNavOpenClose=()=>{
+    let SideNavState = this.state.SideNavState;
+    let ContentOverState = this.state.ContentOverState;
+    if(SideNavState==="sideNavOpen"){
+         this.setState({SideNavState:"sideNavClose",ContentOverState:"ContentOverlayClose"})
+
+    }
+    else{
+         this.setState({SideNavState:"sideNavOpen",ContentOverState:"ContentOverlayOpen"})
+    }
+}
+
   render() {
     return (
       <Fragment>
@@ -13,7 +45,9 @@ class NavMenuDesktop extends Component {
           <Container fluid={"true"} className="fixed-top shadow-sm p-2 mb-0 bg-white">
               <Row>
                 <Col lg={4} md={4} sm={12} xs={12}>
-                  <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
+                <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i>  </Button>
+
+<Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
                 </Col>
 
                 <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
@@ -39,7 +73,13 @@ class NavMenuDesktop extends Component {
 
           </Navbar>
         </div>
+        <div className={this.state.SideNavState}>
+                <MegaMenuAll />
+          </div>
 
+               <div onClick={this.ContentOverlayClickHandler} className={this.state.ContentOverState}>
+
+               </div>
       </Fragment>
     )
   }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,26 @@
 import React, { Component, Fragment } from 'react'
 import NavMenuDesktop from '../components/common/NavMenuDesktop'
+import NavMenuMobile from '../components/common/NavMenuMobile'
 import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import FeatureProducts from '../components/home/FeatureProducts'
 import HomeTop from '../components/home/HomeTop'
+import HomeTopMobile from '../components/home/HomeTopMobile'
 import NewArrival from '../components/home/NewArrival'
 
  class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        <NavMenuDesktop />
-        <HomeTop />
+        <div className="Desktop">
+          <NavMenuDesktop />
+          <HomeTop />
+        </div>
+
+        <div className="Mobile">
+          <NavMenuMobile />  
+          <HomeTopMobile />
+        </div> 
         <FeatureProducts />
         <NewArrival />
         <Collection />
