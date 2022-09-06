44c4360cd4135d1b44df6be30c487f52851de062
mozammal89 <mozammal.bubt89@gmail.com>
2022-09-07 02:56:25 +0600

    consume site info

--- src/api/AppURL.jsx
+++ src/api/AppURL.jsx
@@ -2,6 +2,7 @@
     static BaseURL = "http://127.0.0.1:8000/api"
     static VisitorDetails = this.BaseURL+"/get-visitor" //get visitor details
     static PostContact = this.BaseURL+"/post-contact"  //post contact
+    static getSiteinfo = this.BaseURL+"/site-info"  //post contact
 }
 
 export default AppURL
--- src/components/common/FooterDesktop.jsx
+++ src/components/common/FooterDesktop.jsx
@@ -3,8 +3,48 @@ import { Col, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Apple from '../../assets/images/apple.png'
 import Google from '../../assets/images/google.png'
+import axios from 'axios';
+import AppURL from '../../api/AppURL';
 
 class FooterDesktop extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+         address: "",
+         copyright: "",
+         facebook: "",
+         twitter: "",
+         instragram: "",
+    }
+}
+
+componentDidMount() {
+    axios.get(AppURL.getSiteinfo).then(response => {
+         let StatusCode = response.status;
+         if (StatusCode == 200) {
+              // console.log(response.data['data'][0]['copyright'])
+              let GetAddress = response.data['data'][0]['address'];
+              this.setState({ address: GetAddress });
+
+              let GetCR = response.data['data'][0]['copyright'];
+              this.setState({ copyright: GetCR });
+
+              let GetFacebook = response.data['data'][0]['address'];
+              this.setState({ facebook: GetFacebook });
+
+              let GetTwitter = response.data['data'][0]['twitter_link'];
+              this.setState({ twitter: GetTwitter });
+
+              let GetInstragram = response.data['data'][0]['instragram_link'];
+              this.setState({ instragram: GetInstragram });
+         }
+    }).catch(error => {
+
+    })
+}
+
+
   render() {
     return (
       <Fragment>
@@ -13,13 +53,11 @@ class FooterDesktop extends Component {
             <Row className="px-0 my-5">
               <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
                 <h5 className="footer-menu-title">OFFICE ADDRESS</h5>
-                <p>1635 Franklin Street Montgomery, Near Sherwood Mall. AL 36104 <br></br>
-                  Email: Support@easylearningbd.com
-                </p>
+                <div dangerouslySetInnerHTML={{ __html: this.state.address }} />
                 <h5 className="footer-menu-title">SOCIAL LINK</h5>
-                <a href=""><i className="fab m-1 h4 fa-facebook"></i></a>
-                <a href=""><i className="fab m-1 h4 fa-instagram"></i></a>
-                <a href=""><i className="fab m-1 h4 fa-twitter"></i></a>
+                <a href={this.state.GetFacebook} target="_blank"><i className="fab m-1 h4 fa-facebook"></i></a>
+                <a href={this.state.GetInstragram} target="_blank"><i className="fab m-1 h4 fa-instagram"></i></a>
+                <a href={this.state.GetTwitter} target="_blank"><i className="fab m-1 h4 fa-twitter"></i></a>
               </Col>
 
               <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
@@ -49,7 +87,7 @@ class FooterDesktop extends Component {
           <Container fluid={true} className="text-center m-0 pt-3 pb-1 bg-dark">
             <Container>
               <Row>
-                <h6 className="text-white">© Copyright 2021 by easy Shop, All Rights Reserved</h6>
+                <h6 className="text-white">{ this.state.copyright }</h6>
               </Row>
             </Container>
           </Container>
